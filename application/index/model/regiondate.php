b831c125622ded54e75c8cf7cf1e39f24ee195ca
anhaipo <anhaipo@fang.com>
2018-05-16 14:36:07 +0800

    demo css测试

---
+++ application/index/model/regiondate.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: User
+ * Date: 2018/5/16
+ * Time: 11:23
+ */
+
+namespace app\index\model;
+
+
+use think\Model;
+
+class regionDate extends Model
+{
+    
+}
